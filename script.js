9f3ec97641b16328cc6023eaccb220d34708f3a5
Hatice Özkaya <hatice@192.168.1.106>
2024-05-16 00:50:45 +0300

    index.html

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("loginButton").onclick = function() {
     window.location.href = "SignIn-SignUp-Form-main/login.html";
 };
+
+document.getElementById("Profile").onclick = function() {
+    window.location.href = "./index2.html";
+};
